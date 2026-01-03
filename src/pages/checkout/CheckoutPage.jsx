2ba4b6a0d7fb1b87f03aa4b928a4aa83d0f9dc85
ngocthinh09 <volengocthinh09@gmail.com>
2026-01-03 18:17:39 +0700

    feat: add page-specific favicons for better visual identification

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -4,6 +4,7 @@ import './CheckoutPage.css';
 function CheckoutPage() {
   return (
     <>
+      <link rel="icon" type="image/svg+xml" href="favicon/cart-favicon.png" />
       <title>Checkout</title>
 
       <CheckoutHeader />
