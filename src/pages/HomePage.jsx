9e3945f2ef4e34c0755402544f5c5aa716e16ba0
ngocthinh09 <volengocthinh09@gmail.com>
2026-01-14 18:54:19 +0700

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,11 @@ import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 function HomePage() {
+  fetch('http://localhost:3000/api/products')
+  .then((response) => {response.json()})
+  .then((data) => {console.log(data)})
+
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="favicon/home-favicon.png" />
