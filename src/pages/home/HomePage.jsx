8e947b8dfcbbd2fc25ff3444c56f4020d08d2b0d
ngocthinh09 <volengocthinh09@gmail.com>
2026-01-16 03:49:00 +0700

    refactor: convert API calls in App, CheckoutPage, and HomePage to async/await for improved readability

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,9 +8,11 @@ function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
+    getHomeData();
   }, []);
 
 
