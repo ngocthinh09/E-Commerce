dcd3317d1fd852e1176847f4accc50f2f8a743f5
ngocthinh09 <volengocthinh09@gmail.com>
2026-01-15 13:50:23 +0700

    feat(Header): pass cart data to Header and calculate total quantity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,12 @@ import SearchIcon from '../assets/images/icons/search-icon.png';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import './Header.css';
 
-function Header() {
+function Header({ cart }) {
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  })
+
   return (
     <div className="header">
       <div className="left-section">
@@ -30,7 +35,7 @@ function Header() {
 
         <NavLink className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src={CartIcon} />
-          <div className="cart-quantity">3</div>
+          <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </NavLink>
       </div>
