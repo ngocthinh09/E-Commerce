c59bd8df59766544e20bef4c469ad8d5d876a64d
ngocthinh09 <volengocthinh09@gmail.com>
2026-01-26 10:43:00 +0700

    refactor: separate delivery options and payment summary fetching into distinct useEffect hooks for better clarity

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -11,13 +11,18 @@ function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get("/api/delivery-options?expand=estimatedDeliveryTime");
+      const response = await axios.get("/api/delivery-options?expand=estimatedDeliveryTime");
       setDeliveryOptions(response.data);
-
-      response = await axios.get("/api/payment-summary");
-      setPaymentSummary(response.data);
     };
     fetchCheckoutData();
+  }, []);
+
+  useEffect(() => {
+    const fetchPaymentSummay = async () => {
+      const response = await axios.get("/api/payment-summary");
+      setPaymentSummary(response.data);
+    }
+    fetchPaymentSummay();
   }, [cart]);
 
   return (
