4dc999011c70938c470877d65b4a4535835b7ac1
ngocthinh09 <volengocthinh09@gmail.com>
2026-02-17 01:00:25 +0700

    fix: simplify Vite configuration by removing dynamic environment loading

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_SERVICE,
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://e-commerce-oqef.onrender.com',
+        changeOrigin: true,
+        secure: true,
       },
     },
-  }
+  },
 })
